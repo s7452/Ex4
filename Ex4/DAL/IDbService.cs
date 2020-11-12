420e916f148fd736ed6faca8139a96ab8d0364b2
gypkid <gypkid@gmail.com>
2020-11-12 10:36:20 +0100

    Add project files.

---
+++ Ex4/DAL/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using Ex4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex4.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
